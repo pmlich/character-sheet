573d8562a691439279cdf4e8a6672ba33a2b20d9
pmlich <84739445+pmlich@users.noreply.github.com>
2021-05-31 20:19:42 +0200

    Add files via upload

---
+++ app-data.js
@@ -0,0 +1,165 @@
+//var global = {};
+var characters_list = {
+	0: {
+	"info": {
+		"info": {
+			"Name": "Peter",
+			"Age": 11,
+			"Description": ""
+			}
+		},
+	"attributes":
+		{
+		"Physical":
+			{
+			"Strength": 2,
+			"Dexterity": 1,
+			"Stamina": 5
+			},
+		"Social":
+			{
+			"Charisma": 3,
+			"Manipulation": 2,
+			"Composure": 5
+			},
+		"Mental":
+			{
+			"Intelligence": 1,
+			"Wits": 4,
+			"Resolve": 1
+		        }
+		}
+	},
+	1: {
+	"info": {
+		"info": {
+			"Name": "Lubo",
+			"Age": 22,
+			"Description": ""
+			}
+		},
+	"attributes":
+		{
+		"Physical":
+			{
+			"Strength": 4,
+			"Dexterity": 3,
+			"Stamina": 3
+			},
+		"Social":
+			{
+			"Charisma": 5,
+			"Manipulation": 4,
+			"Composure": 1
+			},
+		"Mental":
+			{
+			"Intelligence": 4,
+			"Wits": 3,
+			"Resolve": 3
+		        }
+		}
+	}
+	};
+
+
+var config =  {
+"blank": {
+	"info": {
+		"info": {
+			"Name": " --- BLANK --- ",
+			"Age": "",
+			"Description": ""
+			}
+		},
+	"attributes":
+		{
+		"Physical":
+			{
+			"Strength": 1,
+			"Dexterity": 1,
+			"Stamina": 1
+			},
+		"Social":
+			{
+			"Charisma": 1,
+			"Manipulation": 1,
+			"Composure": 1
+			},
+		"Mental":
+			{
+			"Intelligence": 1,
+			"Wits": 1,
+			"Resolve": 1
+		        }
+		},
+	"skills":
+		{
+		"Physical":
+			{
+			"Athletics": 0,
+			"Melee": 0,
+			"Larceny": 0,
+			"Shooting": 0,
+			"Casting": 0,
+			"Riding": 0,
+			"Flying": 0,
+			"Brewing": 0,
+			"Stealth": 0,
+			"Survival1": 0,
+			"Brawl": 0,
+			"Survival2": 0
+			},
+		"Social":
+			{
+			"Empathy": 0,
+			"Intimidation": 0,
+			"Persuasion": 0,
+			"Subterfuge": 0,
+			"Animal Ken": 0,
+			"Expression": 0,
+			"Etiquette": 0
+			},
+		"Mental":
+			{
+			"Awareness": 0,
+			"Investigation": 0,
+			"Ancient Runes": 0,
+			"Herbology": 0,
+			"Magic Theory": 0,
+			"Potions": 0,
+			"Magical Creatures": 0,
+			"Crafts": 0
+		        }
+		},
+	"spells":
+		{
+		"Physical":
+			{
+			"push": 0,
+			"shield": 0,
+			"rope": 0,
+			"disarm": 0,
+			"magic arm": 0
+			},
+		"Social":
+			{
+			"charm": 0,
+			"intimidate": 0,
+			"social defense charm": 0
+			},
+		"Mental":
+			{
+			"examine magic": 0,
+			"mind defense": 0,
+			"mind attack": 0
+		        },
+		"Elemental":
+			{
+			"fire bolt": 0,
+			"lightning": 0,
+			"freeze": 0
+		        }
+		}
+	}
+};
