6d036ebb61af7890d313b215196e6292ed3cc78a
Pavel Mlích <83418237+longnicknam3@users.noreply.github.com>
2022-01-03 20:27:23 +0100

    dots hover refactor | color refactor (#99)

--- src/components/Dots.vue
+++ src/components/Dots.vue
@@ -17,48 +17,7 @@ export default {
   },
   props: ['initialValue', 'value', 'scale'],
   emits: ['valueChange'],
-  computed: {
-    /**
-     * @returns {Array} of dots which will be added if we click on dot cursor is hovering over
-     */
-    addPointHover() {
-      var list = [];
-      if (this.hoveringOver != null) {
-        for (let i = this.initialValue + 1; i <= this.scale; i++) {
-          if (this.hoveringOver > this.value && i <= this.hoveringOver) {
-            list.push(i);
-          }
-          if (this.hoveringOver < this.value && i <= this.hoveringOver) {
-            list.push(i);
-          }
-        }
-      }
-      //console.log("add list: " + list)
-      return list;
-    },
-    /**
-     * @returns {Array} of dots which will be removed if we click on dot cursor is hovering over
-     */
-    deletePointHover() {
-      var list = [];
-      if (this.hoveringOver != null) {
-        for (let i = this.initialValue + 1; i <= this.scale; i++) {
-          if (this.hoveringOver == this.value && i <= this.value) {
-            list.push(i);
-          }
-          if (
-            this.hoveringOver < this.value &&
-            i > this.hoveringOver &&
-            i <= this.value
-          ) {
-            list.push(i);
-          }
-        }
-      }
-      //console.log("remove list: " + list)
-      return list;
-    },
-  },
+  
   methods: {
     /**
      * should notify parent component of value change
@@ -68,11 +27,12 @@ export default {
      *    else it will be set
      */
     emitValueChange(i) {
-      i > this.initialValue
-        ? this.value == i
-          ? this.$emit('valueChange', this.initialValue)
-          : this.$emit('valueChange', i)
-        : this.$emit('valueChange', i);
+      let emited_value = i;
+      if (i < this.initialValue || this.value == i) {
+          emited_value = this.initialValue;
+        }
+      this.$emit('valueChange', emited_value);
+          
     },
   },
 };
@@ -87,8 +47,8 @@ export default {
         point: true,
         init: i <= initialValue,
         fill: i > initialValue && i <= value,
-        add: addPointHover.includes(i),
-        remove: deletePointHover.includes(i),
+        add:  i > value && i <= hoveringOver,
+        remove: hoveringOver && i > hoveringOver || hoveringOver == value,
       }"
       @click="emitValueChange(i)"
       @mouseover="hoveringOver = i"
@@ -121,10 +81,10 @@ export default {
 }
 
 .point.add {
-  background: radial-gradient(#ff6666, #ff6666);
-}
+  background: radial-gradient(#ff6666, #ff5050);
+  }
 
-.point.remove {
-  background: radial-gradient(#ff9999, #ff9999);
+.point.fill.remove {
+  background: radial-gradient(#ffffff, #cc0000);
 }
 </style>
